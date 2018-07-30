495b3cbd1879c45bb8b4d5d80d8640c3d3716943
Aaron Whitfield <aawhitfield@hotmail.com>
2018-07-30 01:02:52 -0400

    added store button functionality need to add final 2 buttons

--- Academia/GameViewController.swift
+++ Academia/GameViewController.swift
@@ -12,30 +12,46 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
-    // set size of buttons to be a function of screen size for adaptive layout
-    var widthMultiplier = 0.0
-    var heightMultiplier = 0.0
-
+    // names buttons from Layout Builder for use in code
     @IBOutlet weak var headClassroomButton: UIButton!
-
+    @IBOutlet weak var storeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // sets the size each button should be based on screen size
         let buttonWidth = Double(self.view.frame.size.width) / 5 
-        let buttonHeight = Double(self.view.frame.size.height) / 3
+        let buttonHeight = Double(self.view.frame.size.height) / 2.25
+        
+        // sets the location each button should be based on screen size
         let classRoomButtonX = Double(self.view.frame.size.width) / 7
         let classRoomButtonY = Double(self.view.frame.size.width) / -7
-            
+        let storeButtonX = Double(self.view.frame.size.width) / 2.75
+        let storeButtonY = Double(self.view.frame.size.width) / -7
+        
+        
+        
+        // sets button title and frame size
         headClassroomButton.setTitle("Test Button", for: .normal)
         headClassroomButton.frame = CGRect(x: 200, y: 50, width: 100, height: 100)
         headClassroomButton.translatesAutoresizingMaskIntoConstraints = false
         
-        let widthConstraints = NSLayoutConstraint(item: headClassroomButton, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: CGFloat(buttonWidth))
-        let heightConstraints = NSLayoutConstraint(item: headClassroomButton, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: CGFloat(buttonHeight))
-        let xConstraints = NSLayoutConstraint(item: headClassroomButton, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: CGFloat(classRoomButtonX))
-        let yConstraints = NSLayoutConstraint(item: headClassroomButton, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: CGFloat(classRoomButtonY))
-        NSLayoutConstraint.activate([heightConstraints,widthConstraints,xConstraints,yConstraints])
+        storeButton.setTitle("Test Button", for: .normal)
+        storeButton.frame = CGRect(x: 200, y: 50, width: 100, height: 100)
+        storeButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        // sets layout constraints to the button
+        let classWidthConstraints = NSLayoutConstraint(item: headClassroomButton, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: CGFloat(buttonWidth))
+        let classHeightConstraints = NSLayoutConstraint(item: headClassroomButton, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: CGFloat(buttonHeight))
+        let class_xConstraints = NSLayoutConstraint(item: headClassroomButton, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: CGFloat(classRoomButtonX))
+        let class_yConstraints = NSLayoutConstraint(item: headClassroomButton, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: CGFloat(classRoomButtonY))
+        NSLayoutConstraint.activate([classHeightConstraints,classWidthConstraints,class_xConstraints,class_yConstraints])
+        
+        let storeWidthConstraints = NSLayoutConstraint(item: storeButton, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: CGFloat(buttonWidth))
+        let storeHeightConstraints = NSLayoutConstraint(item: storeButton, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: CGFloat(buttonHeight))
+        let store_xConstraints = NSLayoutConstraint(item: storeButton, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: CGFloat(storeButtonX))
+        let store_yConstraints = NSLayoutConstraint(item: storeButton, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: CGFloat(storeButtonY))
+        NSLayoutConstraint.activate([storeHeightConstraints,storeWidthConstraints,store_xConstraints,store_yConstraints])
 
 
         

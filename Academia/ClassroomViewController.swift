f911dbd576538541531fe5a168bef9c498d53660
Aaron Whitfield <aawhitfield@hotmail.com>
2018-08-31 00:36:34 -0400

    got the timer to work! :-)

--- Academia/ClassroomViewController.swift
+++ Academia/ClassroomViewController.swift
@@ -13,19 +13,41 @@ class ClassroomViewController: UIViewController {
     @IBOutlet weak var timerLabel: UILabel!
     
     var timer = Timer()
-    var time = 10
+    var minutes = 3
+    var seconds = 0
     
     @objc func decreaseTimer()    // timer function to decrement time by one each second
         // added @objc to get selector to run
     {
-        if time >= 0
+        if minutes >= 0      // timer will run while time is still on the clock
         {
-            time -= 1
-            timerLabel.text = String(time)
+            if seconds == 0     // sets when to reset seconds at the start of next minute
+            {
+                minutes -= 1    // decrements minutes when switches to next minute
+                seconds = 59    // resets seconds at the start of the next minute
+            }
+            else
+            {
+                seconds -= 1                    // decrements timer by one
+            }
+            if seconds >= 10
+            {
+                timerLabel.text = String(minutes) + ":" + String(seconds)   // formats seconds for two digit numbers
+            }
+            else
+            {
+                timerLabel.text = String(minutes) + ":0" + String(seconds)  // formats seconds for one digit numbers
+                if seconds == 0     // sets when to reset seconds at the start of next minute
+                {
+                    minutes -= 1    // decrements minutes when switches to next minute
+                    seconds = 60    // resets seconds at the start of the next minute
+                }
+            }
             
         }
         else
         {
+            timerLabel.text = String(minutes) + ":0" + String(seconds)
             timer.invalidate()  // stops timer when reaches zero
         }
     }
@@ -34,6 +56,14 @@ class ClassroomViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if seconds >= 10
+        {
+            timerLabel.text = String(minutes) + ":" + String(seconds)   // formats seconds for two digit numbers
+        }
+        else
+        {
+            timerLabel.text = String(minutes) + ":0" + String(seconds)  // formats seconds for one digit numbers
+        }
         
         // start timer countdown
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ClassroomViewController.decreaseTimer), userInfo: nil, repeats: true)
